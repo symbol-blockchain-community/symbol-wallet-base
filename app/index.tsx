c18a05b646d697ff89d8b55bb4bcebfaa254720a
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2024-06-06 07:07:23 +0900

    chore: Update gitignore and app.json, and update dependencies in package.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,90 @@
 import { Link, useNavigation, useRouter } from 'expo-router';
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
 import ButtonBase from '@/components/atom/ButtonBase';
 import { List } from '@/components/atom/List';
 import { useLoadWallets } from '@/hooks/useLoadWallets';
 import { WalletModel } from '@/models/AccountModel';
 // FIXME import { AddressService } from '@/services/AddressService';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import Button from '@/components/atom/Button';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      handleRegistrationError('Permission not granted to get push token for push notification!');
+      return;
+    }
+    const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError('Project ID not found');
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e: unknown) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError('Must use physical device for push notifications');
+  }
+}
 
 function Item({ item }: { item: WalletModel }) {
   const router = useRouter();
@@ -39,6 +117,12 @@ export default function Root(): React.JSX.Element {
   const { isLoading, wallets } = useLoadWallets();
   const [isWalletsInfoReload, setIsWalletsInfoReload] = React.useState<boolean>(false);
 
+  // add push notification
+  const [expoPushToken, setExpoPushToken] = React.useState('');
+  const [notification, setNotification] = React.useState<Notifications.Notification | undefined>(undefined);
+  const notificationListener = React.useRef<Notifications.Subscription>();
+  const responseListener = React.useRef<Notifications.Subscription>();
+
   React.useEffect(() => {
     const state = navigation.getState();
     navigation.reset({
@@ -46,6 +130,12 @@ export default function Root(): React.JSX.Element {
       routes: state.routes.map((route) => ({ ...route, state: undefined })),
     });
   }, []);
+
+  const testPushNotification = async () => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token || ''))
+      .catch((error) => setExpoPushToken(error));
+  };
 
   const reloadAccountInfo = () => {
     setIsWalletsInfoReload(true);
@@ -67,6 +157,7 @@ export default function Root(): React.JSX.Element {
           <Link href='/_sitemap' className='text-blue-700 underline text-center py-10 text-lg'>
             開発用 - サイトマップへ
           </Link>
+          <Button onPress={testPushNotification}>tset</Button>
           <List
             items={wallets}
             renderItem={(item) => <Item item={item} />}
