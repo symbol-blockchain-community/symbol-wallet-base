893ec81554123675826e17608a01c56833f26c9b
oe-trader <51394769+oe-trader@users.noreply.github.com>
2024-06-08 18:04:16 +0900

    TransactionService,TransferTransactionServeのv3化

--- services/Transactions/TransactionService.ts
+++ services/Transactions/TransactionService.ts
@@ -1,13 +1,21 @@
-import { Account } from 'symbol-sdk/dist/src/model/account';
-import { NetworkType as HexNetworkType, TransactionFees } from 'symbol-sdk/dist/src/model/network';
-import { SignedTransaction, Transaction } from 'symbol-sdk/dist/src/model/transaction';
-
 import { ConnectionError, InvalidValueError } from '@/models/ErrorModels';
-import { NetworkProperty, NetworkType, NodeInfo } from '@/models/NetworkModels';
-import { TransactionFeeRate } from '@/models/TransactionModel';
-import { Configuration, TransactionRoutesApi } from '@/services/NodeClientService';
+import { NetworkProperty, NodeInfo } from '@/models/NetworkModels';
+
 import { NETWORK_PROPERTIES } from '@/util/configs/network-properties';
-import { strNetworkTypeToHexadecimal } from '@/util/symbol/network';
+
+import { PrivateKey, Signature } from 'symbol-sdk';
+import { KeyPair, models, SymbolFacade, Network } from 'symbol-sdk/symbol';
+import { AddressService } from '@/services/AddressService';
+
+export interface ITransactionFees {
+  averageFeeMultiplier: number;
+  medianFeeMultiplier: number;
+  highestFeeMultiplier: number;
+  lowestFeeMultiplier: number;
+  minFeeMultiplier: number;
+}
+
+export type TTransactionFeeRate = 'zero' | 'slow' | 'normal' | 'fast';
 
 export interface TransactionOption {
   /** デッドラインを Hour で指定。デフォルト = 2 */
@@ -18,18 +26,18 @@ export interface TransactionOption {
 
 export class TransactionService {
   protected networkPropeties: NetworkProperty;
-  protected hexNetworkType: HexNetworkType;
+  protected network: Network;
 
-  protected constructor(networkType: NetworkType) {
-    this.networkPropeties = NETWORK_PROPERTIES[networkType];
-    this.hexNetworkType = strNetworkTypeToHexadecimal(networkType);
+  protected constructor(network: Network) {
+    this.networkPropeties = NETWORK_PROPERTIES[network.name as 'testnet' | 'mainnet'];
+    this.network = network;
   }
 
   /**
    * MaxFee を試算する。
    * 乗数は Symbol Desktop Wallet の計算式と統一する https://github.com/symbol/desktop-wallet/blob/00213e53ebed157d091d70fc9ba01aba1822eb9a/src/components/MaxFeeAndSubmit/MaxFeeAndSubmit.vue#L116
    */
-  protected static calcTransactionFee(rate: TransactionFeeRate, fee: TransactionFees, size: number): number {
+  protected static calcTransactionFee(rate: TTransactionFeeRate, fee: ITransactionFees, size: number): number {
     if (rate === 'zero') {
       return 0;
     } else if (rate === 'slow') {
@@ -45,59 +53,90 @@ export class TransactionService {
 
   /**
    * トランザクションへ署名する
+   * @returns {string} Payload
    */
-  public signTransaction(privateKey: string, transaction: Transaction): SignedTransaction {
-    const account = Account.createFromPrivateKey(privateKey, this.hexNetworkType);
-    return account.sign(transaction, this.networkPropeties.generationHashSeed);
+  public signTransaction(privateKey: string | PrivateKey, transaction: models.Transaction): Signature {
+    const keyPair = new KeyPair(typeof privateKey === 'string' ? new PrivateKey(privateKey) : privateKey);
+    const facade = new SymbolFacade(this.network);
+    return facade.signTransaction(keyPair, transaction);
   }
 
   /**
    * トランザクションを同期的にアナウンスする
-   * symbol-sdk v2 の TransactionService 相当の動作をする
    */
-  public async announceTransaction(node: NodeInfo, transaction: SignedTransaction): Promise<Error | any> {
-    const observedAddress: string = transaction.getSignerAddress().plain();
-    const config = new Configuration({ basePath: node.restGatewayUrl });
-    const transactionRoutesApi = new TransactionRoutesApi(config);
+  public async announceTransaction(
+    node: NodeInfo,
+    transaction: models.Transaction,
+    signature: Signature
+  ): Promise<Error | any> {
+    const facade = new SymbolFacade(this.network);
+    const txHash = facade.hashTransaction(transaction).toString();
+    const payload = facade.transactionFactory.static.attachSignature(transaction, signature);
 
-    return await new Promise(function (resolve, reject) {
-      const ws = new WebSocket(node.websocketUrl);
-
-      ws.onopen = () => {
-        // アナウンスは WebSocket を開いた後でなければならない
-        transactionRoutesApi.announceTransaction({ transactionPayload: { payload: transaction.payload } }).catch(() => {
-          ws.close();
-          reject(new ConnectionError('Failed to announce transaction'));
-        });
-      };
-
-      ws.onclose = () => {};
-      ws.onmessage = (message: any) => {
-        // 新規 Confirmed Transaction を監視
-        const res = JSON.parse(message.data);
-        const statusFlag: string = `status/${observedAddress}`;
-        const confirmedFlag: string = `confirmedAdded/${observedAddress}`;
-        if ('uid' in res) {
-          const body1: { [key: string]: string } = { uid: res.uid, subscribe: statusFlag };
-          const body2: { [key: string]: string } = { uid: res.uid, subscribe: confirmedFlag };
-          ws.send(JSON.stringify(body1));
-          ws.send(JSON.stringify(body2));
-        } else {
-          if (res.topic === statusFlag) {
-            // 受け入れられなかった時
-            if (res.data.hash === transaction.hash) {
-              ws.close();
-              reject(new InvalidValueError(res.data.code));
-            }
-          } else if (res.topic === confirmedFlag) {
-            // 成功時
-            if (res.data.meta.hash === transaction.hash) {
-              ws.close();
-              resolve(res);
-            }
-          }
+    // TODO 検証用にfetchで行う
+    return await new Promise(async (resolve, reject) => {
+      try {
+        const sendRes = await fetch(`${node.restGatewayUrl}/transactions`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: payload,
+        }).then((res) => res.json());
+        console.log(sendRes);
+        try {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          const statusRes = await fetch(`${node.restGatewayUrl}/transactionStatus/${txHash}`).then((res) => res.json());
+          console.log(statusRes);
+          resolve(sendRes);
+        } catch (e) {
+          console.error(e);
+          reject(new InvalidValueError(sendRes?.data?.code));
         }
-      };
+      } catch (e) {
+        console.error(e);
+        reject(new ConnectionError('Failed to announce transaction'));
+      }
     });
+
+    // TODO Websocketによるアナウンス方法が良く分からなったのでfetchで検証した
+    // return await new Promise(function (resolve, reject) {
+    //   const ws = new WebSocket(node.websocketUrl);
+
+    //   ws.onopen = () => {
+    //     // アナウンスは WebSocket を開いた後でなければならない
+    //     // FIXME ここが良くわからない。。。
+    //     // transactionRoutesApi.announceTransaction({ transactionPayload: { payload } }).catch(() => {
+    //     //   ws.close();
+    //     //   reject(new ConnectionError('Failed to announce transaction'));
+    //     // });
+    //   };
+
+    //   ws.onclose = () => {};
+    //   ws.onmessage = (message: any) => {
+    //     // 新規 Confirmed Transaction を監視
+    //     const res = JSON.parse(message.data);
+    //     const statusFlag: string = `status/${observedAddress}`;
+    //     const confirmedFlag: string = `confirmedAdded/${observedAddress}`;
+    //     if ('uid' in res) {
+    //       const body1: { [key: string]: string } = { uid: res.uid, subscribe: statusFlag };
+    //       const body2: { [key: string]: string } = { uid: res.uid, subscribe: confirmedFlag };
+    //       ws.send(JSON.stringify(body1));
+    //       ws.send(JSON.stringify(body2));
+    //     } else {
+    //       if (res.topic === statusFlag) {
+    //         // 受け入れられなかった時
+    //         if (res.data.hash === txHash) {
+    //           ws.close();
+    //           reject(new InvalidValueError(res.data.code));
+    //         }
+    //       } else if (res.topic === confirmedFlag) {
+    //         // 成功時
+    //         if (res.data.meta.hash === txHash) {
+    //           ws.close();
+    //           resolve(res);
+    //         }
+    //       }
+    //     }
+    //   };
+    // });
   }
 }
--- services/Transactions/TransferTransactionService.ts
+++ services/Transactions/TransferTransactionService.ts
@@ -1,72 +1,91 @@
-import { Account, Address } from 'symbol-sdk/dist/src/model/account';
-import { EmptyMessage, Message } from 'symbol-sdk/dist/src/model/message';
-import { Mosaic } from 'symbol-sdk/dist/src/model/mosaic';
-import { TransactionFees } from 'symbol-sdk/dist/src/model/network';
-import { Deadline, SignedTransaction, TransferTransaction } from 'symbol-sdk/dist/src/model/transaction';
+// import { Account, Address } from 'symbol-sdk/dist/src/model/account';
+// import { EmptyMessage, Message } from 'symbol-sdk/dist/src/model/message';
+// import { Mosaic } from 'symbol-sdk/dist/src/model/mosaic';
+// import { TransactionFees } from 'symbol-sdk/dist/src/model/network';
+// import { Deadline, SignedTransaction, TransferTransaction } from 'symbol-sdk/dist/src/model/transaction';
 
-import { NetworkType, NodeInfo } from '@/models/NetworkModels';
-import { TransactionFeeRate } from '@/models/TransactionModel';
-import { TransactionOption, TransactionService } from '@/services/Transactions/TransactionService';
+// import { NetworkType, NodeInfo } from '@/models/NetworkModels';
+// import { TransactionFeeRate } from '@/models/TransactionModel';
+import {
+  ITransactionFees,
+  TTransactionFeeRate,
+  TransactionOption,
+  TransactionService,
+} from '@/services/Transactions/TransactionService';
 import { strNetworkTypeToHexadecimal } from '@/util/symbol/network';
+
+import { PrivateKey, PublicKey, Signature } from 'symbol-sdk';
+import {
+  KeyPair,
+  descriptors,
+  models,
+  SymbolFacade,
+  Address,
+  Network,
+  SymbolPublicAccount,
+  SymbolAccount,
+} from 'symbol-sdk/symbol';
+import { AddressService } from '@/services/AddressService';
+import { NodeInfo } from '@/models/NetworkModels';
 
 export interface TransferTransactionOption extends TransactionOption {
   /** 送信先Base32アドレス */
-  recipientAddress: string;
+  recipientAddress: string | Address;
   /** 送信する Mosaic */
-  mosaics: Mosaic[];
+  mosaics: models.Mosaic[];
   /** トランザクションメッセージ */
-  message?: Message;
+  message?: string | Uint8Array;
 }
 
 export class TransferTransactionService extends TransactionService {
-  private transaction: TransferTransaction;
+  private transaction: models.Transaction;
+  private facade: SymbolFacade;
 
   /** TransferTransaction を作成する */
-  public constructor(networkType: NetworkType, options: TransferTransactionOption) {
+  public constructor(networkType: Network, fromAccount: SymbolAccount, options: TransferTransactionOption) {
     super(networkType);
-    this.transaction = TransferTransaction.create(
-      Deadline.create(this.networkPropeties.epochAdjustment, options.deadlineHour || 2),
-      Address.createFromRawAddress(options.recipientAddress),
-      options.mosaics,
-      options.message || EmptyMessage,
-      this.hexNetworkType
-    ).setMaxFee(options.maxFee || 100) as TransferTransaction;
+
+    this.facade = new SymbolFacade(networkType);
+    const transferTransactionDescripter = new descriptors.TransferTransactionV1Descriptor(
+      typeof options.recipientAddress === 'string' ? new Address(options.recipientAddress) : options.recipientAddress,
+      options.mosaics.map((m) => new descriptors.MosaicDescriptor(m.mosaicId, m.amount)),
+      options.message ? TransferTransactionService.prependNullByte(options.message) : undefined
+    );
+
+    this.transaction = this.facade.createTransactionFromTypedDescriptor(
+      transferTransactionDescripter,
+      fromAccount.publicKey,
+      options.maxFee || 100,
+      Number(this.facade.now().addHours(1).timestamp)
+    );
   }
 
   /**
    * MaxFee を試算する。 fee は事前に http://node-domain/network/fees/transaction より取得する。
    */
-  public static calcFee(rate: TransactionFeeRate, fee: TransactionFees, message?: Message): number {
-    const dummyNetworkType = strNetworkTypeToHexadecimal('mainnet');
-    const dummyAccount = Account.generateNewAccount(dummyNetworkType);
+  public static calcFee(rate: TTransactionFeeRate, fee: ITransactionFees, message?: string | Uint8Array): number {
+    const dummyFacade = new SymbolFacade(Network.MAINNET);
+    const dummyAccount = dummyFacade.createAccount(PrivateKey.random());
     // message 以外は計算式に影響しない
-    const size: number = TransferTransaction.create(
-      Deadline.create(0, 0),
+    const transferTransactionDescripter = new descriptors.TransferTransactionV1Descriptor(
       dummyAccount.address,
       [],
-      message || EmptyMessage,
-      dummyNetworkType
+      message ? this.prependNullByte(message) : undefined
+    );
+
+    const size = dummyFacade.createTransactionFromTypedDescriptor(
+      transferTransactionDescripter,
+      dummyAccount.publicKey,
+      0,
+      0
     ).size;
+
     return this.calcTransactionFee(rate, fee, size);
   }
 
   /** @overload 署名する */
-  public signTransaction(privateKey: string): SignedTransaction {
+  public signTransaction(privateKey: string) {
     return super.signTransaction(privateKey, this.transaction);
-  }
-
-  /** @overload トランザクションを同期的にアナウンスする */
-  public async announceTransaction(node: NodeInfo, signedTx: SignedTransaction): Promise<any> {
-    try {
-      const res = await super.announceTransaction(node, signedTx);
-      if (res instanceof Error) {
-        throw res;
-      }
-      return res;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
   }
 
   /**
@@ -77,7 +96,7 @@ export class TransferTransactionService extends TransactionService {
     return {
       v: 3,
       type: this.transaction.type,
-      network_id: this.transaction.networkType,
+      network_id: this.transaction.network.value,
       chain_id: this.networkPropeties.generationHashSeed,
       data: {
         payload: this.transaction.serialize(),
@@ -88,5 +107,24 @@ export class TransferTransactionService extends TransactionService {
   /** 現在のトランザクションを取得する */
   public get data() {
     return this.transaction;
+  }
+
+  /** メッセージの先頭バイトに\0を追加する */
+  private static prependNullByte(input: string | Uint8Array): string | Uint8Array {
+    if (typeof input === 'string') {
+      // 先頭に\0がついていない場合、\0を追加
+      return input.startsWith('\0') ? input : `\0${input}`;
+    } else if (input instanceof Uint8Array) {
+      // Uint8Arrayの場合、先頭に\0を追加
+      if (input[0] !== 0) {
+        const result = new Uint8Array(input.length + 1);
+        result.set([0], 0); // 先頭に\0を追加
+        result.set(input, 1); // 既存の内容を後ろにコピー
+        return result;
+      }
+      return input;
+    } else {
+      throw new Error('Input must be a string or Uint8Array');
+    }
   }
 }
