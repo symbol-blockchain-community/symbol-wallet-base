e886bb2d22b504182e00cfd0e1d4b3945998c189
oe-trader <51394769+oe-trader@users.noreply.github.com>
2024-07-05 00:12:48 +0900

    NetworkTypeのstateを修正

--- states/types.ts
+++ states/types.ts
@@ -1,11 +1,14 @@
+import { NetworkType } from '@/models/NetworkModels';
 // アクションの型定義
 export type Action =
   | { type: 'LOAD_STATE'; payload: AppState }
   | { type: 'SET_NODE'; payload: string }
-  | { type: 'SET_NETWORK_TYPE'; payload: number };
+  | { type: 'GET_NODE'; payload: string }
+  | { type: 'SET_NETWORK_TYPE'; payload: NetworkType }
+  | { type: 'GET_NETWORK_TYPE' };
 
 // 状態の型定義
 export interface AppState {
   node: string;
-  networkType: number;
+  networkType: NetworkType;
 }
